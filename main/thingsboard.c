93f3a71ccc142b56295413ed9162124e1039be70
sarathi <sarathi@trakster.de>
2025-05-24 16:30:18 +0530

    Initial commit with files

---
+++ main/thingsboard.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+void app_main(void) {
+    while ( 1 )
+    {
+        ESP_LOGI( "TAG", "Hello world.starting point of thingsboard mqtt!" );
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    
+}
